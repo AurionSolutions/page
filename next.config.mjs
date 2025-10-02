c968587920eb5d491fda8451fec2f9c3fee281a2
unknown <aurionsolutions.ofc@gmail.com>
2025-10-02 17:42:51 -0300

    chore: deploy build commands

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,12 @@
 // next.config.mjs
 /** @type {import('next').NextConfig} */
-const repoName = '/my-portf-main' // troque conforme necessário
+const repoName = '/page' // nome do repositório no GitHub Pages
 
 const nextConfig = {
-  output: 'export',
-  trailingSlash: true,
-  basePath: repoName,
-  assetPrefix: repoName,
-  // outras opções...
+  output: 'export',       // exporta arquivos estáticos
+  trailingSlash: true,    // gera index.html dentro de pastas
+  basePath: repoName,     // basePath = nome do repo
+  assetPrefix: repoName,  // garante CSS/JS apontando para o caminho certo
 }
 
 export default nextConfig
